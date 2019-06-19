bff953d8d87e813c0ea309049c389609d51c9125
Olumide Ogundele <olumideralph@gmail.com>
2019-06-19 06:58:09 +0100

    create a sparse file

--- sparse/sparse.go
+++ sparse/sparse.go
@@ -1,5 +1,50 @@
 package main
 
-func main()  {
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
+func main() {
 	arguments := os.Args
+
+	if len(arguments) != 3 {
+		fmt.Printf("usage: %s SIZE filename \n", filepath.Base(arguments[0]))
+		os.Exit(1)
+	}
+
+	SIZE, err := strconv.ParseInt(arguments[1], 10, 64)
+	filename := arguments[2]
+
+	_, err = os.Stat(filename)
+	if err == nil {
+		fmt.Printf("File %s already exists. \n", filename)
+		os.Exit(1)
+	}
+
+	fd, err := os.Create(filename)
+	if err != nil {
+		log.Fatal("Failed to create file")
+	}
+
+	_, err = fd.Seek(SIZE - 1, 0)
+	if err != nil {
+		fmt.Println("err")
+		log.Fatal("Failed to seek on file")
+	}
+
+	_, err = fd.Write([]byte{0})
+	if err != nil {
+		fmt.Println("err")
+		log.Fatal("Write operation failed")
+	}
+	err = fd.Close()
+	if err != nil {
+		fmt.Println("err")
+		log.Fatal("Failed to close file")
+	}
+
 }
