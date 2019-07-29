926598fc9cd7ddbfdac6bc22abec2ac9224acde3
Olumide Ogundele <olumideralph@gmail.com>
2019-07-29 06:53:53 +0100

    create a server and mutliplexer to handle multiple request to different paths

---
+++ web/router/router.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func about(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the /about page at %s \n", r.URL.Path)
+	fmt.Printf("Served: %s \n", r.Host)
+}
+
+func cv(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the /cv page at %s \n", r.URL.Path)
+	fmt.Printf("Served: %s \n", r.Host)
+}
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	currentTime := time.Now().Format(time.RFC1123)
+	title := currentTime
+	Body := "The current time is: "
+	fmt.Fprintf(w, "<h1 align=\"center\">%s</h1><h2 align=\"center\">%s</h2>",
+	 Body, title)
+	fmt.Printf("Served: %s for %s \n", r.URL.Path, r.Host)
+}
+
+func home(w http.ResponseWriter, r *http.Request)  {
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "Welcome to the home page \n")
+	} else {
+		fmt.Fprintf(w, "Unknown page: %s from %s \n", r.URL.Path, r.Host)
+	}
+
+	fmt.Printf("Served: %s for %s \n", r.URL.Path, r.Host)
+}
+
+func main()  {
+	m := http.NewServeMux()  // like a factory that produces new multiplexer
+	m.HandleFunc("/about", about)
+	m.HandleFunc("/cv", cv)
+	m.HandleFunc("/time", timeHandler)
+	m.HandleFunc("/", home)
+
+	http.ListenAndServe(":8001", m)
+}
